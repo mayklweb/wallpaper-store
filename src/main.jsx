df8449938681e78f681a8613d62d3c57e649f4b0
mayklweb <muhammadevvv@gmail.com>
2024-11-18 12:44:33 +0500

    wallpaper store

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './assets/style/main.css'
+import "./assets/style/main.css"
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.querySelector('.wrapper')).render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    
-    extend: {
-      fontFamily: {
-        sans: ['Arsenal', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
